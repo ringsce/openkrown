9e88c899beaeb064ce28f3cc27e7f3ae2caee26e
Pedro Dias Vicente <pedrolvicente@gmail.com>
2022-11-30 16:26:22 +0000

    fixing dtd

--- dtd/test.cpp
+++ dtd/test.cpp
@@ -6,13 +6,13 @@ xml_document<> doc;    // character type defaults to char
 doc.parse<0>(text);    // 0 means default parse flags
 int main()
 {
-cout << "Name of my first node is: " << doc.first_node()->name() << "\n";
+std::cout << "Name of my first node is: " << doc.first_node()->name() << "\n";
 xml_node<> *node = doc.first_node("foobar");
-cout << "Node foobar has value " << node->value() << "\n";
+std::cout << "Node foobar has value " << node->value() << "\n";
 for (xml_attribute<> *attr = node->first_attribute();
      attr; attr = attr->next_attribute())
 {
-    cout << "Node foobar has attribute " << attr->name() << " ";
-    cout << "with value "  << attr->value() << "\n";
+    std::cout << "Node foobar has attribute " << attr->name() << " ";
+    std::cout << "with value "  << attr->value() << "\n";
 }
 }
